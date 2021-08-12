89a6b03c0088d36cafa838b59e3b6d4e9ef11d32
Caiofran <caio.ricciardi@soulasalle.com.br>
2021-08-12 20:46:46 -0300

    Semana 1

--- Semana1/Tabuada.java
+++ Semana1/Tabuada.java
@@ -1,10 +1,9 @@
 package Semana1;
 
+// Caio França Ricciardi 0050015558
+
 public class Tabuada {
     public static void main(String[] args) {
-        /*
-         * Caio França Ricciardi 0050015558
-         */
 
         for (int i = 0; i <= 10; i++) {
             System.out.println(10 * i);
