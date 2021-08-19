81f9ad172ff85213317c24bc894413881d844ed9
Caiofran <caio.ricciardi@soulasalle.com.br>
2021-08-19 20:21:44 -0300

    Semana 2

--- Semana2/LerNome.java
+++ Semana2/LerNome.java
@@ -9,34 +9,30 @@ public class LerNome {
         int idade = 0;
         Scanner teclado = new Scanner(System.in);
 
-        //solicita o nome pelo teclado
+        // solicita o nome pelo teclado
         System.out.println("Entre com o nome:");
 
-        //ler nome
+        // ler nome
         nome = teclado.next();
 
-        //Ler Idade
+        // Ler Idade
         System.out.println("Qual sua idade:");
         // idade = Integer.parseInt(teclado.next());
         idade = teclado.nextInt();
 
-        //Imprime saudação
+        // Imprime saudação
         System.out.println("Olá " + nome + ", boa noite!!!");
-        System.out.printf("Você tem %d anos de idade\n",idade);
+        System.out.printf("Você tem %d anos de idade\n", idade);
 
         // Testa idade
-        if(idade<18)
-        {
+        if (idade < 18) {
             System.out.println("Você não pode entrar!!!");
-        }
-        else
-        {
+        } else {
             System.out.println("Acesso permitido.");
         }
 
-        for (int i = 0; i < 10; i++) 
-        {
-           System.out.println(i); 
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
         }
 
         teclado.close();
