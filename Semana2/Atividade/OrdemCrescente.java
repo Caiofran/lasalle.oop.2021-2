ed2dc4b777a6872db679c1ec7d8c763160c1df22
Caiofran <caio.ricciardi@soulasalle.com.br>
2021-08-20 09:34:20 -0300

    Atividade Semana 2

--- Semana2/Atividade/OrdemCrescente.java
+++ Semana2/Atividade/OrdemCrescente.java
@@ -8,12 +8,11 @@ import java.util.Scanner;
 public class OrdemCrescente {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] num = new int[3];
+        int[] num = new int[10];
 
         //ler numeros pelo teclado
         for(int i=0; i<num.length; i++)
         {
-            //System.out.println("Entre com o numero " + (i+1) + ":");
             System.out.printf("\nEntre com o número %d :" , (i+1));
             num[i] = sc.nextInt();
         }
