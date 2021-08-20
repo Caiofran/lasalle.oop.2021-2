9593ff8c8264d5f5e55175f8f2c29c91a6d108a6
Caiofran <caio.ricciardi@soulasalle.com.br>
2021-08-19 21:19:29 -0300

    Semana2

---
+++ Semana2/Numpar.java
@@ -0,0 +1,15 @@
+package Semana2;
+
+public class Numpar {
+    public static void main(String[] args) {
+        // Imprimir numeros pares de 1-100
+        for (int i = 1; i <= 100; i++) 
+        {
+            if(i%2==0)
+            {
+                // System.out.println(i);
+                System.out.print(i + " - ");
+            }
+        }
+    }
+}
