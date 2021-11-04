2abcd12631875593024aa3d5519d9d32a84e3afb
Caiofran <caio.ricciardi@soulasalle.com.br>
2021-11-04 15:50:08 -0300

    update

---
+++ Semana8/Atividade/NuvemTester.java
@@ -0,0 +1,30 @@
+package Semana8.Atividade;
+
+import java.util.Collections;
+import java.util.List;
+// Faça um programa em java para ler um arquivo texto qualquer, calcular a nuvem de palavras e exibir 
+// as 10 maiores na tela.
+
+public class NuvemTester {
+    public static void main(String[] args) {
+        Arquivo arq = new Arquivo("palavra.csv");
+        // List<Voto> votos = arq.apurarVotos();
+
+        // // imprime votos lidos
+        // for (Voto voto : votos) {
+        //     System.out.println(voto);
+        // }
+
+        // Collections.sort(votos);
+
+        // System.out.println("Votos ordenados ****");
+        // // imprime votos lidos ORDENADA
+        // for (Voto voto : votos) {
+        //     System.out.println(voto);
+        // }
+
+        // Definir o candidato vencedor!!!
+
+        // Imprimir ranking final
+    }
+}
