ddfb5a574c5cde93d8b9ba71582f96ff4ecc008e
Caiofran <caio.ricciardi@soulasalle.com.br>
2021-11-04 17:51:47 -0300

    update da semana8 atividade

--- Semana8/Atividade/NuvemTester.java
+++ Semana8/Atividade/NuvemTester.java
@@ -1,6 +1,5 @@
 package Semana8.Atividade;
 
-import java.util.Collections;
 import java.util.List;
 // Faça um programa em java para ler um arquivo texto qualquer, calcular a nuvem de palavras e exibir 
 // as 10 maiores na tela.
